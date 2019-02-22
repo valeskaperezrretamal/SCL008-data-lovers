24b1a940a088225fbcd2d1d98c723f15e5bd53ed
valeskaperezrretamal <jperezr@udd.cl>
2019-02-22 10:58:25 -0300

    se le dio funcionalidad, se agrego forEach, innerHTML, y función de click

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
 /* Manejo del DOM */
+/* agrega elementos a los listados*/
+//Paises
+let Countries = GenerateList(WORLDBANK);
+let OpcionesPaises="";
+Countries.forEach(element => {
+    OpcionesPaises = OpcionesPaises + "<option id='"+ element + "'>'" + element+ "'</option>";
+});
+
+document.getElementById("idCountries").innerHTML = OpcionesPaises;
+//indicadores
+
+let Indicators = GenerateSubList(WORLDBANK[Countries[0]].indicators,"indicatorName");
+Indicators.sort();
+let OpcionesIndicators="";
+Indicators.forEach(element => {
+    OpcionesIndicators = OpcionesIndicators + "<option id='"+ element + "'>'" + element+ "'</option>";
+});
+
+document.getElementById("idIndicators").innerHTML = OpcionesIndicators;
+
+//
+let Indicators1 = GenerateSubList(WORLDBANK[Countries[1]].indicators,"indicatorName");
+Indicators1.sort();
+let OpcionesIndicators1="";
+Indicators1.forEach(element => {
+    OpcionesIndicators1 = OpcionesIndicators1 + "<option id='"+ element + "'>'" + element+ "'</option>";
+});
+
+document.getElementById("idIndicators1").innerHTML = OpcionesIndicators1;
