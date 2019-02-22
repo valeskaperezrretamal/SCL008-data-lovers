8c2da1e021f082955c3bbf54a70d43353342534e
valeskaperezrretamal <jperezr@udd.cl>
2019-02-22 10:52:01 -0300

    Se agrego función para ordenar y mostrar la data de Mexico y Peru

--- src/data.js
+++ src/data.js
@@ -8,3 +8,25 @@ const example = () => {
 };
 
 window.example = example;
+
+GenerateList = (obj)  =>{
+  Arroutput=[];
+  for (prop in obj){
+      Arroutput.push(prop);
+  };
+  return Arroutput;
+};
+
+GenerateSubList = (obj,subprop)  =>{
+  Arroutput=[];
+  for (prop in obj){
+      Arroutput.push(obj[propGenerateSubList = (obj,subprop)  =>{
+        Arroutput=[];
+        for (prop in obj){
+            Arroutput.push(obj[prop][subprop]);
+        };
+        return Arroutput;
+      };[subprop]);
+  };
+  return Arroutput;
+};
