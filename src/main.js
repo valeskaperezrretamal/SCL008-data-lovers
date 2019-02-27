52a1c0e187652d6f671d9deaf5296a92123c8c05
valeskaperezrretamal <jperezr@udd.cl>
2019-02-27 12:51:21 -0300

    se crean funciones: función que llena el <selec> identificado con el id indicado como parametro, con los elementos del array list, función para actualizar los valores de la selección que el usuario seleccionó, se gurda lo seleccionado en estas variables, Actualiza el listado de indicadores de acuerdo a la selección del usuario, define variables para os objetos de html y lo mismo se hace para lo mismo de country pero con la selección de genero

--- src/main.js
+++ src/main.js
@@ -1,13 +1,54 @@
 
 
+let actualCountry;
+let actualGender;
+let actualIndicator;
+let Indicators;
+//Funciones
 
-//
+// función que llena el <selec> identificado con el id indicado como parametro, con los elementos del array list
 
-let Indicators = GenerateSubList(WORLDBANK.CHL.indicators,"indicatorName");
-Indicators.sort();
-let OpcionesIndicators="";
-Indicators.forEach(element => {
-    OpcionesIndicators = OpcionesIndicators + "<option id='"+ element + "'>'" + element+ "'</option>";
-});
+const fillList = (list,id) =>{
+    let htmlcodes="";
+    list.forEach(element => {
+        htmlcodes = htmlcodes+ "<option id="+ element + ">" + element+ "</option>";
+    })
 
-document.getElementById("idIndicators").innerHTML = OpcionesIndicators;
+    document.getElementById(id).innerHTML = htmlcodes;
+    }
+    //función para actualizar los valores de la selección que el usuario seleccionó, se gurda lo seleccionado en estas variables
+const updateSelection=()=>{
+    actualCountry = selectCountry.options[selectCountry.selectedIndex].value;
+    actualGender = selectGender.options[selectGender.selectedIndex].value;
+    actualIndicator = selectGender.options[selectGender.selectedIndex].value;
+}
+// Actualiza el listado de indicadores de acuerdo a la selección del usuario 
+const updateIndicators=()=>{
+    Indicators = GenerateSubList(WORLDBANK[actualCountry].indicators,"indicatorName");
+    Indicators.sort();
+    if(actualGender==="Hombre"){Indicators=Indicators.filter(filterForMen);}
+    if(actualGender==="Mujer"){Indicators=Indicators.filter(filterForWomen);}
+    fillList(Indicators,"selectIndicators");
+}
+
+//-----------------------
+// define variables para os objetos de html
+const selectCountry=document.getElementById("selectCountry");
+const selectGender=document.getElementById("selectGender");
+const selectIndicators=document.getElementById("selectIndicators");
+selectCountry.addEventListener("click", ()=>{
+    updateSelection();//actualiza las variables que guarda la selección del usuario
+    updateIndicators(); //actualiza el listado de indicadores de la etiqueta selec a partir de las variables que guardan la selección del usuario
+    document.getElementById("selectIndicators").style.display = "block"; // hace visible el select de indicadores
+
+})
+//lo mismo de country pero con la selección de genero
+selectGender.addEventListener("click", ()=>{
+    updateSelection();
+    updateIndicators();
+    document.getElementById("selectIndicators").style.display = "block";   
+})
+selectIndicators.addEventListener("click", ()=>{
+    
+})
+
