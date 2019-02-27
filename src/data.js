7cf2512afe13ccf14e299e1bd2d9df11dd9e573c
TaphieM <taffymayen@gmail.com>
2019-02-26 18:50:47 -0600

    prueba1 función filtrar

--- src/data.js
+++ src/data.js
@@ -9,21 +9,30 @@ const example = () => {
 
 window.example = example;
 //funcion en la cual entra un objeto y sale un array con los elementos del objeto 
-const GenerateList = (obj)  =>{
-  let Arroutput=[];
-  for (prop in obj){
-      Arroutput.push(prop);
-  }
-  return Arroutput;
-};
+//const GenerateList = (obj)  =>{
+//  let Arroutput=[];
+//  for (prop in obj){
+//      Arroutput.push(prop);
+//  }
+//  return Arroutput;
+//};
 //funcion en la cual entra un objeto y una propiedad de ese objeto o elemento del objeto y devueve un array con los valores de la propiedad para cada elemento del objeto
-const GenerateSubList = (obj,subprop)  =>{
-  let Arroutput=[];
-  for (prop in obj){
-    Arroutput.push(obj[prop][subprop]);
+//const GenerateSubList = (obj,subprop)  =>{
+//  let Arroutput=[];
+//  for (prop in obj){
+//    Arroutput.push(obj[prop][subprop]);
     
-  };
-  return Arroutput;
+//  };
+//  return Arroutput;
 
   
-};
+//};
+
+const filterCountries = (data, condition) => {
+  let listIndicators = data.filter(element => {
+    return element.indicatorName.includes(condition) === true;
+  })
+  return listIndicators;
+}
+
+window.filterCountries = filterCountries;
--- src/main.js
+++ src/main.js
@@ -1,13 +1,44 @@
 
 
 
-//
 
-let Indicators = GenerateSubList(WORLDBANK.CHL.indicators,"indicatorName");
-Indicators.sort();
-let OpcionesIndicators="";
-Indicators.forEach(element => {
-    OpcionesIndicators = OpcionesIndicators + "<option id='"+ element + "'>'" + element+ "'</option>";
+
+//let Indicators = GenerateSubList(WORLDBANK.CHL.indicators,"indicatorName");
+//Indicators.sort();
+//let OpcionesIndicators="";
+//Indicators.forEach(element => {
+//    OpcionesIndicators = OpcionesIndicators + "<option id='"+ element + "'>'" + element+ "'</option>";
+//});
+
+//document.getElementById("idIndicators").innerHTML = OpcionesIndicators;
+
+
+
+const dataPer=WORLDBANK.PER.indicators;                                     //guardo en una variable la ruta de la data a la que quiero accesar
+const indicatorsPer = document.getElementById("indicatorsByCountry");       //elementos que quiero manipular
+
+const filterPeru=(dataPer) =>{                                             //creo una función que usa la data y le aplica forEach
+
+    dataPer.forEach(element =>{
+
+        indicatorsPer.innerHTML += element.indicatorName        //Imprimo cada elemento 
+        
+     })
+}
+
+document.getElementById("indicators").addEventListener("change", () => {
+    let condition = document.getElementById("indicators").value;          //guardo el valor de la data
+    let country = condition.substring(0,6);
+    let listOfIndicators = window.filterCountries(dataPer,country);
+
+    indicatorsPer.innerHTML ="";                                       // vacio para que se limpie en cada evento
+    listOfIndicators.forEach(element =>{
+
+        indicatorsPer.innerHTML += element.indicatorName          //Imprimo cada elemento 
+        
+     }) 
+
+
 });
 
-document.getElementById("idIndicators").innerHTML = OpcionesIndicators;
+window.onload= filterPeru(dataPer);
