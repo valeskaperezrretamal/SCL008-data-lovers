e92b5d7bebc7b936fbd5e45dae383807bb04f68b
valeskaperezrretamal <jperezr@udd.cl>
2019-03-05 00:28:51 -0300

    se crea pruebas para funciones computeMean, compueMax, COmputeMin, ComuteMedian

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,69 +1,72 @@
-global.window = global;
-global.assert = require('chai').assert;
-require('../src/data');
-require('./data.spec.js');
+//global.window = global;
+//global.assert = require('chai').assert;//
+//require('../src/data');//
+//nrequire('./data.spec.js');
 
 
-describe('example', () => {
-  
-  it('debería ser una función', () => {
-    assert.equal(typeof example, 'function');
+describe('', () => {
+    
+  // Para comprobar la función computeMean,que calcula el promedio de la segunda columna, tiene que soportar valores vacios, de entrada tiene un array con dos columnas y de salida un promedio de la segunda columna, se salta y no concidera los elementos vacios 
+  describe('computeMean', () => {
+      //que voy a testear, se puede hacer para saber si es función, para saber si me devulve lo que quiero
+    it('debería ser una función',() => {
+      // se prueba que es una función
+      assert.equal(typeof computeMean, "function");
+    });
+    //se prueb una función con un array
+    it('ingresa este array [[1960,""],[1987,21],[1899,98],[2002,45],[2010,70],[2017,""]] debe retornar 58.5', () => {
+      assert.equal(computeMean([[1960,""],[1987,21],[1899,98],[2002,45],[2010,70],[2017,""]]), 58.5);
+    });
   });
 
-  it('debería retornar "example"', () => {
-    assert.equal(example(), 'example');
+  // Para comprobar la función computeMean,que calcula el promedio de la segunda columna, tiene que soportar valores vacios, de entrada tiene un array con dos columnas y de salida un promedio de la segunda columna, se salta y no concidera los elementos vacios 
+  describe('computeMax', () => {
+    //que voy a testear, se puede hacer para saber si es función, para saber si me devulve lo que quiero
+  it('debería ser una función',() => {
+    // se prueba que es una función
+    assert.equal(typeof computeMax, "function");
   });
-})
-
-//Testing para la función filtrar por pais
-
-describe("countryFilter"),() => {
-  
-  const data =       //creo una constante con poca data por practicidad.
-  [
-    {
-      countryName: "Perú",
-      indicatorName: "Empleo de tiempo parcial, mujeres (% del total de mujeres empleadas)",
-    },
-    {
-      countryName: "México",
-      indicatorName: "Prevalencia de anemia entre mujeres no embarazadas (% de mujeres entre 15-49 años)",
-    },
-    {
-      countryName:"Brasil",
-      indicatorName:"La ley exige igualdad de remuneración para hombres y mujeres por trabajo de igual valor (1=sí; 0=no)",
-    },
-    {
-      countryName: "Chile",
-      indicatorName: "Fuerza laboral con educación intermedia (% del total)",
-    }
-  ]
-
-
-  it("debería ser una función",() => {
-    assert.equal(typeof countryFilter, "function");
-
+  //se prueb una función con un array
+  it('ingresa este array [[1960,""],[1987,21],[1899,98],[2002,45],[2010,70],[2017,""]] debe retornar 98', () => {
+    assert.equal(computeMax([[1960,""],[1987,21],[1899,98],[2002,45],[2010,70],[2017,""]]), 98);
   });
+});
 
-  it("debería retornar el objeto Empleo de tiempo parcial, mujeres (% del total de mujeres empleada al filtrar",() => {
-    assert.deepEqual(window.countryFilter, (data,"Perú"),  //usamos deepEqual para objetos y arrays ya que sino arroja error (===)
-    [{countryName: "Perú", indicatorName:"Empleo de tiempo parcial, mujeres (% del total de mujeres empleadas)"}] )  //esta linea porque se pone?
-  });
+// Para comprobar la función computeMean,que calcula el promedio de la segunda columna, tiene que soportar valores vacios, de entrada tiene un array con dos columnas y de salida un promedio de la segunda columna, se salta y no concidera los elementos vacios 
+describe('computeMin', () => {
+  //que voy a testear, se puede hacer para saber si es función, para saber si me devulve lo que quiero
+it('debería ser una función',() => {
+  // se prueba que es una función
+  assert.equal(typeof computeMin, "function");
+});
+//se prueb una función con un array
+it('ingresa este array [[1960,""],[1987,21],[1899,98],[2002,45],[2010,70],[2017,""]] debe retornar 21', () => {
+  assert.equal(computeMin([[1960,""],[1987,21],[1899,98],[2002,45],[2010,70],[2017,""]]), 21);
+});
+});
 
-  it("debería retornar el objeto Prevalencia de anemia entre mujeres no embarazadas (% de mujeres entre 15-49 años) al filtrar",() => {
-    assert.deepEqual(window.countryFilter,(data,"México"),
-    [{countryName:"México", indicatorName:"Prevalencia de anemia entre mujeres no embarazadas (% de mujeres entre 15-49 años)"}])
-  });
+// Para comprobar la función computeMean,que calcula el promedio de la segunda columna, tiene que soportar valores vacios, de entrada tiene un array con dos columnas y de salida un promedio de la segunda columna, se salta y no concidera los elementos vacios 
+describe('computeMedian', () => {
+  //que voy a testear, se puede hacer para saber si es función, para saber si me devulve lo que quiero
+it('debería ser una función',() => {
+  // se prueba que es una función
+  assert.equal(typeof computeMedian, "function");
+});
+//se prueb una función con un array
+it('ingresa este array [[1960,""],[1987,21],[1899,98],[2002,45],[2010,70],[2017,""]] debe retornar 57.5', () => {
+  assert.equal(computeMedian([[1960,""],[1987,21],[1899,98],[2002,45],[2010,70],[2017,""]]), 57.5);
+});
+});
 
-  it("debería retornar el objeto La ley exige igualdad de remuneración para hombres y mujeres por trabajo de igual valor (1=sí; 0=no) al filtrar", () =>{
-    assert.deepEqual(window.countryFilter,(data,"Brasil"),
-    [{countryName:"Brasil", indicatorName:"La ley exige igualdad de remuneración para hombres y mujeres por trabajo de igual valor (1=sí; 0=no)"}])
-  });
 
-  it("debería retornar el objeto Fuerza laboral con educación intermedia (% del total) al filtrar",() =>{
-    assert.deepEqual(window.countryFilter,(data,"Chile"),
-    [{countryName:"Chile", indicatorName:"Fuerza laboral con educación intermedia (% del total)"}])
-  })
 
-} 
-//Termina testing función filtrar por país
+
+
+
+
+
+
+
+});
+
+
